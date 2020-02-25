888a139c9a381987622b4f0fb6a9b7c54f7a42e2
semantic-release <semantic-release>
2020-02-25 14:18:14 +0000

    0.0.9
    Automatically generated by python-semantic-release

--- spine_json_lib/__init__.py
+++ spine_json_lib/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 __url__ = 'https://github.com/socialpoint-labs/spine-json-lib'
 
 from spine_json_lib.spine_animation_editor import SpineAnimationEditor
